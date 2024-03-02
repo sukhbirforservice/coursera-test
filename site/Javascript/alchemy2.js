0a6026b6f2a94fc2bd0c63ef3c457f9a2490fc69
Sukhbir Singh <89540293+sukhbirforservice@users.noreply.github.com>
2024-03-03 02:22:01 +0530

    Update alchemy2.js

--- site/Javascript/alchemy2.js
+++ site/Javascript/alchemy2.js
@@ -105,3 +105,22 @@ function maxSum(num) {
 }
 
 module.exports = maxSum;
+
+// Replace John, A string is stored in the variable fakeName. Take this fake name and use it to replace every occurrence of "John" in the message., Do not change the message in other way.
+
+const fakeName = require('./fakeName');
+
+const message = `
+    Hello, ${fakeName}! You left a package at the office today.
+    You can pick up tomorrow at 10am, ${fakeName}. 
+    If not I will drop it off this weekend.
+    Goodbye ${fakeName}!
+`;
+
+module.exports = message;
+
+
+
+
+
+
