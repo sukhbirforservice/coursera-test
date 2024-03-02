02c278e4a579114693972bed84e298f1e54fdd7e
Sukhbir Singh <89540293+sukhbirforservice@users.noreply.github.com>
2024-03-03 02:28:20 +0530

    Update alchemy2.js

--- site/Javascript/alchemy2.js
+++ site/Javascript/alchemy2.js
@@ -119,8 +119,23 @@ const message = `
 
 module.exports = message;
 
+// The function checkNumber takes a single argument: a number num , The function should return the string positive if the number is positive, negative if the number is negative, and zero if the number is zero.
 
+function checkNumber(num) {
+    if (num < 0) {
+        return "negative";
+    }
+    else if (num <= 0) {
+        return "zero";
+    }
 
+    else {
+        return "positive";
+    }
+}
+
+module.exports = checkNumber;
+ 
 
 
 
