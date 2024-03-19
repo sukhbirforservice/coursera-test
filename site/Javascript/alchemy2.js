f02ed187206b4044c93d3213e1170610130f9dcb
Sukhbir Singh <89540293+sukhbirforservice@users.noreply.github.com>
2024-03-20 03:43:45 +0530

    Update alchemy2.js

--- site/Javascript/alchemy2.js
+++ site/Javascript/alchemy2.js
@@ -765,3 +765,52 @@ a += string[i];
 }
 
 module.exports = reverse;
+
+// Palindrome
+
+Write a function isPalindrome that takes a word string and returns true if the word is a palindrome or false if it is not.
+
+A palindrome is a word that is spelled the same forwards as it is backwards.
+
+Mine
+function isPalindrome(string) {
+        for (let i = 0; i <= string.length; i++) {
+            if (string[i] == string[string.length - 1]) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+
+module.exports = isPalindrome;
+
+Theres
+
+function isPalindrome(string) {
+    return string === reverse(string);
+}
+
+function reverse(string) {
+    let newStr = "";
+
+    for (let i = string.length - 1; i >= 0; i--) {
+        newStr += string[i]
+    }
+
+    return newStr;
+}
+
+module.exports = isPalindrome;
+
+// Sum Together
+
+function sumTogether(arr1, arr2) {
+    let newArray = [];
+    for (let i = 0; i < arr1.length; i++){
+        element = arr1[i] + arr2[i];
+        newArray.push(element);
+    } return newArray;
+}
+
+module.exports = sumTogether;
