4fff6a4bd0d12d6ab245f3e0357f1d6efa74f3b0
Sukhbir Singh <89540293+sukhbirforservice@users.noreply.github.com>
2024-03-21 03:16:52 +0530

    Update alchemy2.js

--- site/Javascript/alchemy2.js
+++ site/Javascript/alchemy2.js
@@ -814,3 +814,20 @@ function sumTogether(arr1, arr2) {
 }
 
 module.exports = sumTogether;
+
+// Count the Elements
+// Write a function countElements that takes in an array and returns an object a count of each element in the array.
+const elements = ["e", "k", "e", "z", "i", "z"];
+countElements(elements); // returns {e: 2, k: 1, z: 2, i: 1}
+
+function countElements(elements){
+    let object = {};
+    for(let i = 0; i < elements.length; i++){
+        if (!object[elements[i]]){
+            object[elements[i]] = 1;
+        } else {
+            object[elements[i]] += 1;
+        }
+    } return object;
+    }
+module.exports = countElements;
