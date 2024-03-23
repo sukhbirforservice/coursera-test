d2500c8bd0ec4851a7f8b701996b0db80a039e33
Sukhbir Singh <89540293+sukhbirforservice@users.noreply.github.com>
2024-03-24 01:32:15 +0530

    Create alchemy3.js

---
+++ site/Javascript/alchemy3.js
@@ -0,0 +1,44 @@
+// Logical Operators
+
+Let's say we wanted to travel to an island by boat or by plane. In English, we use or in the previous sentence to express we're willing to take either mode of transportation.
+
+In JavaScript, we can use ||:
+
+const boat = true;
+const plane = false;
+
+const willTravel = boat || plane;
+
+console.log(willTravel); // true
+In the example above, one of the conditions is true in the OR operation (the boat), therefore the value stored in willTravel is true!
+
+The || operator is commonly referred to as the Logical OR because it will evaluate to true if either (or both) of the inputs are true.
+
+  
+if(car) {
+    driveToAirport();
+}
+else if(motorcycle) {
+    driveToAirport();
+}
+else if(truck) {
+    driveToAirport();
+}
+
+If we needed to change the driveToAirport function, we would have to change it in 3 places. 
+
+ It's good practice to DRY up your code. This stands for Don't Repeat Yourself. You should always try to avoid code redundancy where possible!
+
+
+Let's try to accomplish the same functionality with the Logical OR (||) operator:
+
+if(car || motorcycle || truck) {
+    driveToAirport();
+}
+Phew, much cleaner! The || operator takes two values and returns true if either boolean value is true:
+
+console.log(true || false); // true
+console.log(false || true); // true
+console.log(true || true); // true
+console.log(false || false); // false
+
