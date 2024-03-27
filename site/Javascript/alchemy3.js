1d856a73cb8de216e9ba6613d3997e15a15b84bd
Sukhbir Singh <89540293+sukhbirforservice@users.noreply.github.com>
2024-03-28 00:51:41 +0530

    Update alchemy3.js

--- site/Javascript/alchemy3.js
+++ site/Javascript/alchemy3.js
@@ -162,3 +162,31 @@ function friendName(friend) {
 }
 
 module.exports = friendName;
+
+// Not Operator
+
+console.log(!true); // false
+console.log(!false); // true
+It will also flip truthy and falsey values:
+
+console.log(!2); // false
+console.log(!undefined); // true
+
+There's a river that runs right between two towns. In order for cars to cross the river, we built a bridge!
+
+Trouble is, there's only room for 1 car to cross the bridge at a time. This means that if both cars were to be crossing at the same time, neither would be able to cross.
+
+Complete the carCrossing function to return true if and only if one car is crossing.
+
+function carCrossing(aCrossing, bCrossing) {
+    if (aCrossing && bCrossing){
+        return false;
+    }
+    return aCrossing || bCrossing;
+}
+
+module.exports = carCrossing;
+
+function carCrossing(aCrossing, bCrossing) {
+    return (aCrossing && !bCrossing) || (bCrossing && !aCrossing);
+}
